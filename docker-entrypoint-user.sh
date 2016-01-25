22faefe3bc1354c54b15475577efcabae77f25b3
Ricky Cook <mail@thatpanda.com>
2016-01-25 18:48:03 +1100

    More robust user creation

--- docker-entrypoint-user.sh
+++ docker-entrypoint-user.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+set -x
+
 function try {
+  while [[ $(awk '$2 ~ /:(6448|3D38|3D3A)/{print}' < /proc/net/tcp | wc -l) < 3 ]]; do
+    sleep 1
+  done
   while ! "$@"; do
     sleep 1
   done
 }
 
-try rabbitmqctl add_user "$FRONTEND_USER" "$FRONTEND_PASSWORD"
-try rabbitmqctl add_user "$BACKEND_USER" "$BACKEND_PASSWORD"
+fe_count=$((try rabbitmqctl -t 1 list_users) | grep "$FRONTEND_USER" | wc -l)
+be_count=$((try rabbitmqctl -t 1 list_users) | grep "$BACKEND_USER" | wc -l)
 
-try rabbitmqctl set_permissions "$FRONTEND_USER" '^$' '^$' '^dockci\.job\..+'
-try rabbitmqctl set_permissions "$BACKEND_USER" dockci dockci dockci
+[[ $fe_count == '0' ]] && rabbitmqctl add_user "$FRONTEND_USER" "$FRONTEND_PASSWORD"
+[[ $be_count == '0' ]] && rabbitmqctl add_user "$BACKEND_USER" "$BACKEND_PASSWORD"
+
+rabbitmqctl set_permissions "$FRONTEND_USER" '^$' '^$' '^dockci\.job\..+'
+rabbitmqctl set_permissions "$BACKEND_USER" dockci dockci dockci
 
 try rabbitmqctl clear_password guest
